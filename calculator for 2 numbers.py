1b0ca8d01a77fe0840a2b5f27e971d2403627ac9
Questelek <questelek@gmail.com>
2026-01-23 18:53:57 +0100

    Add files via upload
    The request time was added to the log
    The calculation and answer output system was slightly reworked and improved

--- calculator for 2 numbers.py
+++ calculator for 2 numbers.py
@@ -1,32 +1,63 @@
-num1 = int(input("Enter a number one: ")) #ask user for the first number
+#function for get time now
+def get_time():
+    import datetime
+    time_now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+    return time_now
 
-question = True #variable to control the loop process
+#function for add data in log
+def log_write(num1, operator, num2, answer, get_time):
+    try:
+        with open("log.txt", "a") as log:
+            if answer is None:
+                log.write(f"{get_time}   /   Question: {num1} {operator} {num2}. Answer: You cannot divide by zero." "\n")
+            else:
+                log.write(f"{get_time}   /   Question: {num1} {operator} {num2}. Answer: {answer}." "\n")
+    except FileNotFoundError:
+        print("File has been not found")
+
+#number asking function
+def get_number(message):
+    while True:
+        try:
+            return int(input(message))
+        except ValueError:
+            print("Please enter a valid number")
 
 valid_operators = ["+", "-", "*", "/", "**", "//"]
 
-while question == True:
-    operator = input(f"Enter operator of {valid_operators}: ") #user need to enter operator
+#ask user for the first number
+num1 = get_number("Enter number one: ")
 
-    if operator in valid_operators:
-        num2 = int(input("Enter a number two - ")) #ask user for the second number
+#user need to enter operator
+operator = input(f"Enter operator of {valid_operators}: ")
+while operator not in valid_operators:
+    operator = input(f"Enter operator of {valid_operators}: ")
 
-        if operator == "+":
-            print(f"Answer: {num1 + num2}")
-        elif operator == "-":
-            print(f"Answer: {num1 - num2}")
-        elif operator == "*":
-            print(f"Answer: {num1 * num2}")
-        elif operator == "/":
-            print(f"Answer: {num1 / num2}")
-        elif operator == "**":
-            print(f"Answer: {num1 ** num2}")
-        elif operator == "//":
-            print(f"Answer: {num1 // num2}")
-        #calculation
-        break
-    else:
-        print("It isn´t a valid operator") #informing user about invalid operator
+# ask user for the second number
+num2 = get_number("Enter number two: ")
 
-    if input("Do you want continue your tries? Enter Yes or No - ") not in ["Yes", "yes", "y", "Y"]:
-        question = False
-    #asking user for continue
+# calculation
+try:
+    if operator == "+":
+        answer = num1 + num2
+    elif operator == "-":
+        answer = num1 - num2
+    elif operator == "*":
+        answer = num1 * num2
+    elif operator == "/":
+        answer = num1 / num2
+    elif operator == "**":
+        answer = num1 ** num2
+    elif operator == "//":
+        answer = num1 // num2
+except ZeroDivisionError:
+    answer = None
+
+#print of answer
+if answer is None:
+    print("You cannot divide by zero")
+else:
+    print(f"Answer: {answer}")
+
+#answer logging
+log_write(num1, operator, num2, answer, get_time())
